a22da17a731b0b00ee842195d22971b5bbf0a11e
Cigarrito RD <maycolk.hernandez@gmail.com>
2023-06-19 20:55:04 -0400

    feat: rutas de plataformas y generos agregadas

--- src/routes/Routers.jsx
+++ src/routes/Routers.jsx
@@ -1,21 +1,25 @@
 
-import React from 'react'
+import { lazy, Suspense } from 'react'
 import { Routes, Route } from 'react-router-dom'
-import GameDetail from '../pages/GameDetail'
-import Home from '../pages/Home'
-import Games from '../pages/Games'
-import Search from '../pages/Search'
+
+const Games = lazy(() => import('../pages/Games'))
+const Home = lazy(() => import('../pages/Home'))
+const GameDetail = lazy(() => import('../pages/GameDetail'))
+const Search = lazy(() => import('../pages/Search'))
 
 const Routers = () => {
   return (
-    <Routes>
-      <Route path='/' element={<Home />} />
-      <Route path='/search/' element={<Search />} />
-      <Route path='/search/:query' element={<Search />} />
-      <Route path='/games' element={<Games />} />
-      <Route path='/games/:genre' element={<Games />} />
-      <Route path='/games/detail/:slug' element={<GameDetail />} />
-    </Routes>
+    <Suspense fallback={<h1>Cargando...</h1>}>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        < Route path='/search/' element={< Search />} />
+        < Route path='/search/:query' element={< Search />} />
+        < Route path='/games' element={< Games />} />
+        < Route path='/games/:name' element={< Games />} />
+        < Route path='/games/:platform/:genre' element={< Games />} />
+        < Route path='/games/detail/:slug' element={< GameDetail />} />
+      </Routes >
+    </Suspense>
   )
 }
 
