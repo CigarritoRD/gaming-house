9a3795ea6930b8af9926f777c51798b75ab8b96e
Cigarrito RD <maycolk.hernandez@gmail.com>
2023-08-18 14:50:42 -0400

    cambio de texto boton de carga

--- src/components/list-of-games.jsx
+++ src/components/list-of-games.jsx
@@ -22,18 +22,16 @@ export function GameList ({ games, getMoreGames, url }) {
           gridTemplateColumns: 'repeat(auto-fit, minmax(310px, 1fr))',
           gap: '14px'
         }}>
-
         {games.map(game => (
           <GameCard key={game.id}
             game={game}
           />
-
         ))}
       </ul>
       {games.length > 0 && <button
         ref={observerdRef}
         onClick={getMoreGames}
-        className='border w-full md:w-fit px-8 mx-auto hover:bg-slate-600 duration-150 p-4 text-slate-100 text-lg rounded-xl bg-slate-700'>Cargar mas...
+        className='border w-full md:w-fit px-8 mx-auto hover:bg-slate-600 duration-150 p-4 text-slate-100 text-lg rounded-xl bg-slate-700'>Cargando mas...
       </button>}
     </>
   )
