d66fb458a52382e7153eeddafcc546c7dc3f2337
Cigarrito RD <maycolk.hernandez@gmail.com>
2023-08-18 14:41:35 -0400

    css

--- src/components/list-of-games.jsx
+++ src/components/list-of-games.jsx
@@ -13,27 +13,28 @@ export function GameList ({ games, getMoreGames, url }) {
     return () => observer.disconnect()
   }, [])
 
-  return (<>
-    <ul
-      ref={rootRef}
-      style={{
-        display: 'grid',
-        gridTemplateColumns: 'repeat(auto-fit, minmax(310px, 1fr))',
-        gap: '14px'
-      }}>
+  return (
+    <>
+      <ul
+        ref={rootRef}
+        style={{
+          display: 'grid',
+          gridTemplateColumns: 'repeat(auto-fit, minmax(310px, 1fr))',
+          gap: '14px'
+        }}>
 
-      {games.map(game => (
-        <GameCard key={game.id}
-          game={game}
-        />
+        {games.map(game => (
+          <GameCard key={game.id}
+            game={game}
+          />
 
-      ))}
-    </ul>
-    {games.length > 0 && <button
-      ref={observerdRef}
-      onClick={getMoreGames}
-      className='border w-full md:w-fit px-8 mx-auto hover:bg-slate-600 duration-150 p-4 text-slate-100 text-lg rounded-xl bg-slate-700'>Cargar mas...
-    </button>}
-  </>
+        ))}
+      </ul>
+      {games.length > 0 && <button
+        ref={observerdRef}
+        onClick={getMoreGames}
+        className='border w-full md:w-fit px-8 mx-auto hover:bg-slate-600 duration-150 p-4 text-slate-100 text-lg rounded-xl bg-slate-700'>Cargar mas...
+      </button>}
+    </>
   )
 }
