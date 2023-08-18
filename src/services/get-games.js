016db9d8450364d3028a3d1914c42dda2f68bed4
Cigarrito RD <maycolk.hernandez@gmail.com>
2023-08-18 14:41:51 -0400

    adapter

--- src/services/get-games.js
+++ src/services/get-games.js
@@ -5,7 +5,28 @@ export async function getGames ({ url }) {
     const res = await fetch(url)
     if (!res.ok) throw new Error('error del servidor')
     const data = await res.json()
-    return data
+    const AdaptedData = data.results.map(game => {
+      return {
+        id: game.id,
+        slug: game.slug,
+        name: game.name,
+        released: game.released,
+        background_image: game.background_image,
+        ratings: game.ratings,
+        ratings_count: game.ratings_count,
+        metacritic: game.metacritic,
+        platforms: game.platforms,
+        parent_platforms: game.parent_platforms,
+        genres: game.genres,
+        stores: game.stores
+      }
+    })
+    return {
+      count: data.count,
+      next: data.next,
+      prevous: data.previous,
+      results: AdaptedData
+    }
   } catch (error) {
     console.log(error)
   }
